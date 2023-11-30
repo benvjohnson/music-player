ee1f0c5819d79825d187fe7f729715d7abb71d85
benvjohnson <benjohnsonv98@gmail.com>
2023-11-30 23:24:48 +0530

    SpringBoot Application Bootstrap

---
+++ src/main/java/com/musicPlayer/musicPlayerApp/MusicPlayerAppApplication.java
@@ -0,0 +1,13 @@
+package com.musicPlayer.musicPlayerApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicPlayerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicPlayerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musicPlayer/musicPlayerApp/MusicPlayerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicPlayer.musicPlayerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicPlayerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
