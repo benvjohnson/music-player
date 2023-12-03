5dd47ba15957b5e6abfeba2c979a5cbbe9a877ac
benvjohnson <benjohnsonv98@gmail.com>
2023-12-03 16:26:52 +0530

    API For the Implementation fo the signIn and Token Email Feature

--- src/main/java/com/musicPlayer/musicPlayerApp/Service/AdminService.java
+++ src/main/java/com/musicPlayer/musicPlayerApp/Service/AdminService.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.musicPlayer.musicPlayerApp.Model.Admin;
+import com.musicPlayer.musicPlayerApp.Model.AutheticationToken;
+import com.musicPlayer.musicPlayerApp.Model.Dto.signInInput;
 import com.musicPlayer.musicPlayerApp.Model.Dto.signInOutput;
 import com.musicPlayer.musicPlayerApp.Repository.AdminRepository;
+import com.musicPlayer.musicPlayerApp.Service.Utility.EmailHandler;
 import com.musicPlayer.musicPlayerApp.Service.Utility.PasswordEncrypter;
 
 @Service
@@ -13,6 +16,14 @@ public class AdminService {
 
     @Autowired
     AdminRepository adminRepo;
+    @Autowired
+    AuthenticationService authService;
+
+    /**
+     * Service Function to Register a Admin
+     * @param admin
+     * @return
+     */
 
     public signInOutput signUpAdmin(Admin admin){
 
@@ -46,4 +57,45 @@ public class AdminService {
             return new signInOutput(signUpStatus,signUpStatusMessage);
         }
     }
+
+    /**
+     * Service Function To Sign in as a Admin 
+     * @param signInput
+     * @return
+     */
+
+    public String signInAdmin(signInInput signInput)
+    {
+        String signInStatusMessage = null;
+        String signInMail =  signInput.getEmail();
+        //check weather the email exists 
+        if( signInMail == null) {
+            signInStatusMessage = "Invalid email";
+            return signInStatusMessage;
+        }
+        Admin existingAdmin = adminRepo.findFirstByAdminEmail(signInMail);
+        if(existingAdmin == null) {
+            signInStatusMessage = "Email not registered!!!";
+            return signInStatusMessage;
+        }
+        try {
+            String encryptedPassword = PasswordEncrypter.encryptPassword(signInput.getPassword());
+            if(existingAdmin.getAdminPassword().equals(encryptedPassword)) {
+                AutheticationToken authToken = new AutheticationToken(existingAdmin);
+                authService.saveAuthToken(authToken);
+                EmailHandler.sendEmail("emailtest@gmail.com","email testing",authToken.getAdminToken());
+                return "Token sent to your email";
+            } else {
+                signInStatusMessage = "Invalid credentials!!!";
+                return signInStatusMessage;
+            }
+
+        } catch (Exception e) {
+            signInStatusMessage = "Internal error occurred during sign in";
+            return signInStatusMessage;
+        }
+
+    }
+
+
 }
